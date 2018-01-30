f11158253eaf154327e9325f00a09228f5b5885a
Blake Kunishige <blake3457@yahoo.com>
2018-01-30 21:28:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return recipes
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes.prop2 = "2"
-  return recipes
+ 
 }
